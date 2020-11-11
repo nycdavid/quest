853baa9fecafde99c65e47c83e5190a86d0ef0c1
David Ko <david.ko@velvetreactor.com>
2020-11-11 09:49:29 -0500

    Adds profile image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Velvet Reactor builds`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `David Ko`,
+      summary: `a software engineer in NJ always looking to optimize.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: "",
     },
   },
   plugins: [
